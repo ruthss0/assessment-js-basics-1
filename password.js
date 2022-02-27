c8719a195012fe27f474af98811268e29041e1af
Ruth Gabriel de Sousa <ruth.sousa15@gmail.com>
2022-02-26 19:17:58 -0700

    create more constraints

--- password.js
+++ password.js
@@ -8,10 +8,29 @@ const reader = readline.createInterface({
 console.log('Welcome to the password validator tool');
 reader.question("What is a password you would like to validate? ", (input) => {
     if(input.length >= 10){
-        console.log("Success: password is greater than 10") 
+        console.log("Success: Password is greater than 10") 
     } else {
-        console.log("failure: Password should be 10 or more characters")
+        console.log("Failure: Password should be 10 or more characters")
     }
-    // This line closes the connection to the command line interface.
+
+    if(/\d/.test(input)) {
+        console.log("Success: Password contains a number")
+    } else {
+        console.log("Failure: Password doesn't contain a number")
+    }
+
+    if(/[A-Z]/.test(input)) {
+        console.log("Success: Password contains a capitalized letter")
+    } else {
+        console.log("Failure: Password doesn't contain a capitalized letter")
+    }
+
+    if(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(input)) {
+        console.log("Success: Password contains a special character")
+    } else {
+        console.log("Failure: Password doesn't contain a special character")
+    }
+    
+    // This line closes the connection to 
     reader.close()
 });
